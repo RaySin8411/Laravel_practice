c4d332d58a221d30dd2bcc58a4cbdd4c880eff6c
Ray Sin <ray.sin@everfortune.ai>
2021-11-02 10:04:36 +0800

    feat(broadcasting): basic setting

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\OrderShipped;
 use Illuminate\Http\Request;
 
 /*
@@ -27,10 +28,19 @@ Route::get('ubike/{sno}', ['as' => 'ubike', 'uses' => 'UbikeController@show']);
 //});
 
 Route::post('register', 'MemberController@store'); //註冊
-Route::post('login', [ 'as' => 'login', 'uses' => 'LoginController@login']); //登入
+Route::post('login', ['as' => 'login', 'uses' => 'LoginController@login']); //登入
 
 
 Route::middleware('auth:api')->get('user', 'MemberController@index');  //查看
 Route::middleware('auth:api')->put('user', 'MemberController@update'); //編輯
 Route::middleware('auth:api')->delete('user/{users}', 'MemberController@destroy'); //刪除
 Route::middleware('auth:api')->get('logout', 'LogoutController@logout'); //登出
+
+Route::get(
+    '/ship',
+    function (Request $request) {
+        $id = $request->input('id');
+        event(new OrderShipped($id)); // 触发事件
+        return Response::make('Order Shipped!');
+    }
+);
