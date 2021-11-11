c829e91967d09cbd1347ee02e7f656b73d679dd0
Ray Sin <ray.sin@everfortune.ai>
2021-11-11 10:36:07 +0800

    feat(excel): basic expert flow

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\UsersExport;
+use Maatwebsite\Excel\Facades\Excel;
+
 /**
  * @group 使用者
  *
@@ -16,4 +19,13 @@ class UserController
         return View::make('admin.login');
     }
 
+    public function export()
+    {
+        return Excel::download(new UsersExport, 'users.xlsx');
+    }
+
+    public function storeExcel()
+    {
+        return Excel::store(new UsersExport, 'users.xlsx', 's3');
+    }
 }
